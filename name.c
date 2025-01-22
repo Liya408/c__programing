7e924f1c58e14d45e9d022fb4f7791c809eb6012
Liya408 <liyabn80@gmail.com>
2025-01-22 16:30:27 +0530

    Add files via upload

---
+++ name.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+  printf("size of flaot=%ld",sizeof(float));
+  printf("size of longint=%ld",sizeof(long int));
+  printf("size if shortint=%ld",sizeof(short int));
+  printf("size of double=%ld",sizeof(double));
+  printf("size if char=%ld",sizeof(char));
+  return 0;
+}
