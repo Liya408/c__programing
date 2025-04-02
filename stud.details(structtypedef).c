83e074d1672d86d3987133bea83043231628e8ed
Liya408 <liyabn80@gmail.com>
2025-04-02 14:43:08 +0530

    Add files via upload

---
+++ stud.details(structtypedef).c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+typedef struct{
+int rollNo;
+char name[20];
+float cgpa;
+}student;
+int main(){
+int n;
+printf("enter the number of students");
+scanf("%d",&n);
+student s[n];
+for(int i=0;i<n;i++){
+printf("\n enter the roll number:");
+scanf("%d",&s[i].rollNo);
+printf("\nenter the name:");
+scanf("%s",s[i].name);
+printf("\nenter the cgpa:");
+scanf("%f",&s[i].cgpa);
+}
+printf("\nThe student details:");
+for(int i=0;i<n;i++){
+printf("\nName:%s",s[i].name);
+printf("\nRollNo:%d",s[i].rollNo);
+printf("\ncgpa:%f",s[i].cgpa);
+}
+return 0;
+}
---
+++ studentdetailsstructure.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+struct student{
+int rollNo;
+char name[20];
+float cgpa;
+};
+int main(){
+struct student s1;
+printf("\n enter the roll number;");
+scanf("%d",&s1.rollNo);
+printf("\nenter the name;");
+scanf("%s",s1.name);
+printf("\nenter the cgpa");
+scanf("%f",&s1.cgpa);
+printf("\nThe student details;");
+printf("\nName%s",s1.name);
+printf("\nRollNO%d",s1.rollNo);
+printf("\ncgpa%f",s1.cgpa);
+return 0;
+}
